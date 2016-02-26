41c197f9c204358a224c8a8e8751e8c14d5704ea
Megan Nilles <darkstarre@hotmail.com>
2016-02-26 13:28:02 -0600

    Basics of the unambiguous search hash

---
+++ lib/hash.rb
@@ -0,0 +1,14 @@
+Dict = {
+add          => ["plus","+" ,"sum"],
+subtract     => ["minus", "-", "difference", "subtraction"],
+divide       => ["division", "quotient","/","%"],
+multiply     => ["multiplication", "*" ,"x" ],
+array        => ["ordered list", "list", "[]" ],
+greater_than => [">", "greater", "greater than",">="],
+less_than    => ["<", "less", "less than" "<="],
+equal        => ["equals", "=", "<=", ">="],
+boolean      => {"true","false","booleans"]
+float        => {"decimal","decimals"]
+
+}
+
