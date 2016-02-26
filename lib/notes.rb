9ef103bacf0eaa7dc30c8df6cd9dc58c79b8e54f
Megan Nilles <darkstarre@hotmail.com>
2016-02-26 14:50:22 -0600

    Updated to parse string and place into array

--- lib/notes.rb
+++ lib/notes.rb
@@ -1,8 +1,16 @@
 def find_notes(notes, thing_to_find)
   if thing_to_find
-    found_thing = notes.scan /.*#{thing_to_find}.*/i
-    found_thing.join
+    found_thing = [] 
+    notes.each  do |arg|
+    found_thing << arg.scan(/.*#{thing_to_find}.*/i)
+    end
+
+    found_thing.join('\n')
   else
-    notes
+    notes.join('\n')
   end
+end
+
+def parse_data(notes)
+  notes.split('\n')
 end
