7c7e5912e4c2b1b6684085ac05ce61c9e0e10213
JamesMarkWilton <james.mark.wilton@gmail.com>
2016-02-29 14:25:07 -0600

    Our Dictionary is built

--- lib/hash.rb
+++ lib/hash.rb
@@ -1,14 +1,23 @@
 Dict = {
-add          => ["plus","+" ,"sum"],
-subtract     => ["minus", "-", "difference", "subtraction"],
-divide       => ["division", "quotient","/","%"],
-multiply     => ["multiplication", "*" ,"x" ],
-array        => ["ordered list", "list", "[]" ],
-greater_than => [">", "greater", "greater than",">="],
-less_than    => ["<", "less", "less than" "<="],
-equal        => ["equals", "=", "<=", ">="],
-boolean      => {"true","false","booleans"]
-float        => {"decimal","decimals"]
-
+"plus" => "add",
+"+" => "add",
+"sum" => "add",
+"minus" => "subtract",
+"-" => "subtract",
+"difference" => "subtract",
+"subtraction" => "subtract",
+"division" => "divide",
+"quotient" => "divide",
+"/" => "divide",
+"%" => "divide",
+"multiplication" => "multiply",
+"*" => "multiply",
+"x" => "multiply",
+"list" => "array",
+"[]" => "array",
+"boolean" => "true"
+"boolean" => "false"
+"decimal" => "float"
+"decimals" => "float"
 }
 
