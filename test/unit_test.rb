abdfbd529a8ce6dcb852af7dfdcd4ffdacc602dc
Javier Revuelta <javier.revult@gmail.com>
2016-02-29 18:39:25 -0600

    Changed data Structure

--- test/unit_test.rb
+++ test/unit_test.rb
@@ -4,15 +4,23 @@ require 'notes'
 class NotesTest < Minitest::Test
 
   def test_you_can_search_the_notes
-    note = ["a", "b", "c"]
-    selectors = ["b"]
-    assert_equal ["b"], find_notes(note, selectors)
+    note = [{"description" => "one", "example" => "1"},
+            {"description" => "two", "example" => "2"},
+            {"description" => "three", "example" => "3"}]
+    selectors = ["one"]
+    assert_equal ["description" => "one", "example" => "1"], find_notes(note, selectors)
+
+    selectors = ["2"]
+    assert_equal [{"description" => "two", "example" => "2"}], find_notes(note, selectors)
   end
 
   def test_search_is_case_insensitive
-    note = ["a", "b", "c"]
-    selectors = ["B"]
-    assert_equal ["b"], find_notes(note, selectors)
+    note = [{"description" => "one", "example" => "1"},
+            {"description" => "two", "example" => "2"},
+            {"description" => "three", "example" => "3"}]
+
+    selectors = ["ONE"]
+    assert_equal [{"description" => "one", "example" => "1"}], find_notes(note, selectors)
   end
 
   def test_no_selector_returns_all_notes
@@ -22,9 +30,16 @@ class NotesTest < Minitest::Test
   end
 
   def test_you_can_search_with_successive_filters
-    note = ["a", "a & b", "a & b & c"]
-    selectors = ["a", "b", "c"]
-    assert_equal ["a & b & c"], find_notes(note, selectors)
+    note = [{"description" => "one", "example" => "1"},
+            {"description" => "one, two", "example" => "1, 2"},
+            {"description" => "one, two, three", "example" => "1, 2, 3"}]
+
+    selectors = ["ONE", "two", "three"]
+    assert_equal [{"description" => "one, two, three", "example" => "1, 2, 3"}], find_notes(note, selectors)
+  end
+
+  def test_you_can_search_for_hidden_data
+    skip
   end
 
 end
